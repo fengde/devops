29aaca9cc8ee49ebb511749e473203500c6143c8
fedelfeng <fedelfeng@tencent.com>
2022-04-03 21:57:38 +0800

    feat: jenkins

---
+++ jenkins/golang_micro_service_template.sh
@@ -0,0 +1,6 @@
+git clone git@github.com:fengde/golang-micro-service-template.git
+app_name=${APP_NAME}
+echo "开始构建"+$app_name
+cp -fr golang-micro-service-template/template $app_name
+tar zcf $app_name.tar.gz $app_name
+printenv
